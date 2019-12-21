d588d8b3131f436f72dd4953690a6f9fac345e1a
Ecem Okan <ecem.okan@softtech.com.tr>
2019-12-21 16:11:16 +0300

    commit master

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+/**
+* RealTime chatApp
+* @author Ecem Okan
+*/
+const mysql = require('mysql');
+ 
+class Db {
+        constructor(config) {
+        this.connection = mysql.createPool({
+            connectionLimit: 100,
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'chat',
+            debug: false
+            });
+        }   
+    
+        query(sql, args) {
+            return new Promise((resolve, reject) => {
+                this.connection.query(sql, args, (err, rows) => {
+                    if (err)
+                    return reject(err);
+                    resolve(rows);
+                });
+            });
+        }
+        close() {
+            return new Promise((resolve, reject) => {
+                this.connection.end(err => {
+                    if (err)
+                    return reject(err);
+                    resolve();
+                });
+            });
+        }
+    }
+    module.exports = new Db();
